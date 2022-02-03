da8c1c6419062e31682398b776088c03ee098613
Edilson Gafanhoto <68605911+edskate@users.noreply.github.com>
2022-02-02 23:55:09 -0300

    nova atualização

---
+++ app.js
@@ -0,0 +1,21 @@
+function listems(items, pageActual, limitItems) {
+  Let result = [];
+  
+  return result[];
+}
+
+let totalpage = Math.ceil(items.length / limitItems );
+
+let count = (pageActual * limitItems ) - limitItems;
+
+let delimiter = count + limitItems;
+
+if(pageActual <= totalPage){
+
+}
+
+for(let i=count; i<delimiter; i++){
+result.push(items[i]);
+count++;
+}
+
